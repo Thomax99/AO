cc30550c7791021153c81fff839cb6f319c8aad5
thmorin <thomas.morin@etu.u-bordeaux.fr>
2021-11-30 09:11:09 +0100

    Revert "test push enzo"
    This reverts commit 9fe50408a44af4721cd8ed4260b501dd70c2d92b.

--- culture_proj/src/Main.java
+++ culture_proj/src/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-//test push Enzo
+
 public class Main extends Application {
 
 	@Override
